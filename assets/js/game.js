210f87c9832d2c868d59eb6dcbfe24b7fcf65096
Francisco Carrera <franciscocarrer@gmail.com>
2017-08-22 02:12:30 -0400

    Added logic to check for preious guesses. Strings and arrays need to be fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,6 +17,7 @@ var lettersGuessed = [];
 var wins = 0;
 var losses = 0;
 var currentWord;
+var triesLeft = 10;
 
 var guessString = [];
 
@@ -52,15 +53,43 @@ function AddLoss() {
     document.getElementById("losses").innerHTML = "Losses: " + losses;
 }
 
-document.addEventListener("keypress", function CheckGuess(e) {
+function HasTried(letter) {
 
+    console.log(lettersGuessed.indexOf(letter));
+    // console.log(lettersGuessed);
+
+    var temp = lettersGuessed.toString().split(" ");
+
+    console.log(temp);
+
+    if(temp.indexOf(letter) == -1){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+document.addEventListener("keypress", function CheckGuess(e) {
     for (var index = 0; index < currentWord.length; index++) {
         if(e.key == currentWord[index]){
             guessString[index] = e.key; 
+        }
+        else if (0 == triesLeft) {
+            AddLoss();
+        }
+        else
+        {
+            triesLeft--;
         }
     }
 
     document.getElementById("word-current").innerHTML = guessString.join(" ");
     console.log(guessString.join(""));
-    
+
+    lettersGuessed = lettersGuessed.concat(e.key + " ");
+    document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(" ");
+
+    console.log(HasTried(e.key));
+
 })
